7eb2fc08e889c2231a6de3e12cfe12f4e7460ac0
TheDomesticUser <thedomesticuser@gmail.com>
2019-06-18 20:12:23 -0400

    Update ui.js
    Replaced innerText in each element with textContent, as it is faster. 2019-06-18

--- Weather/ui.js
+++ Weather/ui.js
@@ -3,14 +3,14 @@ class UI
     static setWeather(weather) {
         // Weather promise is resolved
         // Get the weather information divs and change their text node to the weather info
-        document.getElementById('w-location').innerText = `${weather.name}, ${weather.sys.country}`;
-        document.getElementById('w-desc').innerText = weather.weather[0].description;
+        document.getElementById('w-location').textContent = `${weather.name}, ${weather.sys.country}`;
+        document.getElementById('w-desc').textContent = weather.weather[0].description;
         document.getElementById('w-icon').src = `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
         // Set all of the list items to the response values
-        document.getElementById('w-humidity').innerText = 'Humidity: ' + weather.main.humidity;
-        document.getElementById('w-pressure').innerText = 'Pressure: ' + weather.main.pressure;
-        document.getElementById('w-description').innerText = 'Description: ' + weather.weather[0].description;
-        document.getElementById('w-wind').innerText = `Wind speed: ${weather.wind.speed}`;
+        document.getElementById('w-humidity').textContent = 'Humidity: ' + weather.main.humidity;
+        document.getElementById('w-pressure').textContent = 'Pressure: ' + weather.main.pressure;
+        document.getElementById('w-description').textContent = 'Description: ' + weather.weather[0].description;
+        document.getElementById('w-wind').textContent = `Wind speed: ${weather.wind.speed}`;
     }
 
     static displayError(errorMessage) {
